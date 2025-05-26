52ed78e6747195fb933ce86c3da00a2c3af7a7d7
Tom Quist <tomquist@users.noreply.github.com>
2025-05-26 21:24:16 +0200

    Remove invalid port logging

--- ha_addon/run.sh
+++ ha_addon/run.sh
@@ -103,10 +103,6 @@ export MQTT_ALLOWED_CONSECUTIVE_TIMEOUTS=$(bashio::config 'allowedConsecutiveTim
 export MQTT_PROXY_ENABLED=$(bashio::config 'mqttProxyEnabled' "false")
 bashio::log.info "MQTT allowed consecutive timeouts: ${MQTT_ALLOWED_CONSECUTIVE_TIMEOUTS}"
 bashio::log.info "MQTT proxy enabled: ${MQTT_PROXY_ENABLED}"
-if [ "${MQTT_PROXY_ENABLED}" = "true" ]; then
-    bashio::log.info "MQTT proxy port: ${MQTT_PROXY_PORT}"
-fi
-
 bashio::log.info "MQTT polling interval: ${MQTT_POLLING_INTERVAL} seconds"
 bashio::log.info "MQTT response timeout: ${MQTT_RESPONSE_TIMEOUT} seconds"
 
