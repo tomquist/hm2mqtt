04f87eec4a183e1be35db07447d4ac9cdc61f0cb
Tom Quist <tomquist@users.noreply.github.com>
2025-07-26 13:11:35 +0200

    Rename FC4 sensor

--- src/device/mi800.ts
+++ src/device/mi800.ts
@@ -330,7 +330,7 @@ function registerRuntimeInfoMessage(message: BuildMessageFn) {
       ['fc4Version'],
       sensorComponent<string>({
         id: 'fc4_version',
-        name: 'FC4 Version',
+        name: 'FC41D Firmware',
         icon: 'mdi:chip',
       }),
     );
